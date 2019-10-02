099b16f1593d295e28abbdc24c9d6a44b3bd4564
Hansol <sori9088@naver.com>
2019-10-02 22:24:46 +0700

    third

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let random = Math.floor(Math.random()*100+1);
-let count = 1;
+let count = 9;
 let history = [];
 console.log(random);
 
@@ -8,6 +8,13 @@ function guess() {
 
     if (history.includes(guessNum)) {
         alert("You already say that number ! ");
+        document.getElementById("guessNum").value='';
+        return;
+    } 
+
+    if (guessNum == ""){
+        alert("Input any number ! ");
+        document.getElementById("guessNum").value='';
         return;
     }
 
@@ -16,14 +23,20 @@ function guess() {
 
 
     if (random > guessNum) {
-        document.getElementById("result").innerHTML = "&#10071; Too Small &#10071;";
+        document.getElementById("alert").setAttribute('class','alert alert-danger');
+        document.getElementById("alert").innerHTML= "&#10071; Too Small &#10071;";
     } else if (random < guessNum) {
-        document.getElementById("result").innerHTML = "&#10071; Too Big &#10071;";
-    } else {
-        document.getElementById("result").innerHTML = "&#11088; Collect &#11088;";
-    }
+        document.getElementById("alert").setAttribute('class','alert alert-danger');
+        document.getElementById("alert").innerHTML = "&#10071; Too Big &#10071;";
+    } else if (random == guessNum){
+        document.getElementById("alert").setAttribute('class','alert alert-success') ;
+        document.getElementById("alert").innerHTML = "&#11088; Collect &#11088;";
+        stopTimer();
+        document.getElementById("guessbtn").disabled=true;
+        
+        }
 
-    document.getElementById("guesses").innerHTML = "Number of attempts : "+(count++);
+    document.getElementById("guesses").innerHTML = `Remaining Guesses : ${(count--)}`;
     
    
 
@@ -36,13 +49,44 @@ function guess() {
 
     }
 
-    if (history.length>=5) {
+    if (count<0) {
         document.getElementById("guessbtn").disabled=true;
-        document.getElementById("string").style.color="red";
+        document.getElementById("string").style.color="#F25F29";
         document.getElementById("string").innerHTML=`The answer is ${random}`;
+        stopTimer();
+        document.getElementById("alert").setAttribute('class','alert alert-danger') ;
+        document.getElementById("alert").innerHTML = "&#128520; You Lose &#128520;";
+
         // console.log("here",histor,"here");
         // let str ="here"+history+"here"
         // innerHTML= `here ${history} here`
     }
     document.getElementById("guessNum").value='';
 }
+
+
+let time = 31;
+let myTime;
+
+function setTime() {
+    time -= 1;
+    document.getElementById("timer").innerHTML = `🕒Timer : ${time}🕒`;
+
+    if(time==0){
+        stopTimer();
+        document.getElementById("guessbtn").disabled=true;
+        document.getElementById("string").style.color="red";
+        document.getElementById("alert").setAttribute('class','alert alert-danger');
+        document.getElementById("alert").innerHTML = "&#10071; Time over &#129322; &#10071;";
+    }
+}
+
+function timeCounting() {
+    myTime = setInterval(setTime, 1000);
+}
+
+function stopTimer(){
+    clearInterval(myTime);
+}
+
+timeCounting();
